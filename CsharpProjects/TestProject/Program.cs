c209e6ad66560dc61db34c22205dfa0d49bd625a
th3p3ngu1n <kristin.b.harris21@gmail.com>
2025-10-12 18:33:52 -0500

    exercise - do while challenge

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,13 +1,18 @@
-﻿for (int i = 1; i <= 100; i++)
+﻿int heroHealth = 10;
+int monsterHealth = 10;
+Random attack = new Random();
+
+do
 {
-  string output = "";
-  if (i % 3 == 0)
-  {
-    output += "Fizz";
-  }
-  if (i % 5 == 0)
-  {
-    output += "Buzz";
-  }
-  Console.WriteLine($"{i}{(output != string.Empty ? $" - {output}" : "")}");
-}
+  int attackPoints = attack.Next(1, 11);
+  monsterHealth -= attackPoints;
+  Console.WriteLine($"Monster was damaged and lost {attackPoints} health and now has {monsterHealth} health.");
+  
+  if (monsterHealth <= 0) continue;
+  
+  attackPoints = attack.Next(1, 11);
+  heroHealth -= attackPoints;
+  Console.WriteLine($"Hero was damaged and lost {attackPoints} health and now has {heroHealth} health.");
+} while (heroHealth > 0 && monsterHealth > 0);
+
+Console.WriteLine(heroHealth > monsterHealth ? "Hero wins!" : "Monster wins!");
