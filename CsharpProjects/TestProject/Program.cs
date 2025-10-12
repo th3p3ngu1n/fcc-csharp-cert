2fba13b26644f547df590caf58f9a0ff3263b7be
th3p3ngu1n <kristin.b.harris21@gmail.com>
2025-10-12 18:49:51 -0500

    do while - code project 1

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,18 +1,18 @@
-﻿int heroHealth = 10;
-int monsterHealth = 10;
-Random attack = new Random();
-
+﻿Console.WriteLine("Please enter a number between 5 and 10");
+int input = 0;
 do
 {
-  int attackPoints = attack.Next(1, 11);
-  monsterHealth -= attackPoints;
-  Console.WriteLine($"Monster was damaged and lost {attackPoints} health and now has {monsterHealth} health.");
-  
-  if (monsterHealth <= 0) continue;
-  
-  attackPoints = attack.Next(1, 11);
-  heroHealth -= attackPoints;
-  Console.WriteLine($"Hero was damaged and lost {attackPoints} health and now has {heroHealth} health.");
-} while (heroHealth > 0 && monsterHealth > 0);
-
-Console.WriteLine(heroHealth > monsterHealth ? "Hero wins!" : "Monster wins!");
+  if (!int.TryParse(Console.ReadLine(), out input))
+  {
+    Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    continue;
+  }
+  if (input < 5 || input > 10)
+  {
+    Console.WriteLine($"You entered {input}. Please enter a number between 5 and 10.");
+  }
+  else
+  {
+    Console.WriteLine($"Your input value ({input}) has been accepted.");
+  }
+} while (input < 5 || input > 10);
