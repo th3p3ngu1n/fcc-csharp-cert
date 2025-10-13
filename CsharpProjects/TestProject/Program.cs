66b4c4c2273341609f45c9f0d2a26f479e69c7ae
th3p3ngu1n <kristin.b.harris21@gmail.com>
2025-10-12 19:03:42 -0500

    do while - code project 2

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,18 +1,20 @@
-﻿Console.WriteLine("Please enter a number between 5 and 10");
-int input = 0;
+﻿string[] validRoles = { "administrator", "manager", "user" };
+string rolePrompt = "Enter your role name (Administrator, Manager, or User)";
+Console.WriteLine(rolePrompt);
+
+string? input = "";
+bool valid = false;
 do
 {
-  if (!int.TryParse(Console.ReadLine(), out input))
+  input = Console.ReadLine();
+  if (!validRoles.Contains(input?.Trim()?.ToLower()))
   {
-    Console.WriteLine("Sorry, you entered an invalid number, please try again");
-    continue;
-  }
-  if (input < 5 || input > 10)
-  {
-    Console.WriteLine($"You entered {input}. Please enter a number between 5 and 10.");
+    Console.WriteLine($"The role name that you entered, \"{input}\" is not valid. {rolePrompt}");
   }
   else
   {
+    valid = true;
     Console.WriteLine($"Your input value ({input}) has been accepted.");
   }
-} while (input < 5 || input > 10);
+
+} while (!valid);
