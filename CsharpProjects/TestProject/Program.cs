d9129d038b544d974d3d6fdd4d4b489921030ee9
th3p3ngu1n <kristin.b.harris21@gmail.com>
2025-10-18 12:56:40 -0500

    exercise - examine tryparse

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,21 +1,12 @@
-﻿decimal myDecimal = 1.23456789m;
-float myFloat = (float)myDecimal;
-
-Console.WriteLine($"Decimal: {myDecimal}");
-Console.WriteLine($"Float  : {myFloat}");
-
-string first = "5";
-string second = "7";
-int sum = int.Parse(first) + int.Parse(second);
-Console.WriteLine(sum);
-
-string value1 = "5";
-string value2 = "7";
-int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
-Console.WriteLine(result);
-
-int value = (int)1.5m; // casting truncates
-Console.WriteLine(value);
-
-int convertedValue = Convert.ToInt32(1.5m); // converting rounds up
-Console.WriteLine(convertedValue);
+﻿string value = "bad";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+  Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+  Console.WriteLine("Unable to report the measurement.");
+}
+if (result > 0)
+  Console.WriteLine($"Measurement (w/ offset): {50 + result}");
