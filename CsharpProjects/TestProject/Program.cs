f2f388d8079489ff6d22d6b490cf19da3fc84e1f
th3p3ngu1n <kristin.b.harris21@gmail.com>
2025-10-19 18:12:37 -0500

    exercise - challenge to reverse words in sentence

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,16 +1,10 @@
-﻿int value1 = 11;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
-
-// Your code here to set result1
-// Hint: You need to round the result to nearest integer (don't just truncate)
-int result1 = Convert.ToInt32(value1 / value2);
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
-
-// Your code here to set result2
-decimal result2 = value2 / Convert.ToDecimal(value3);
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
-
-// Your code here to set result3
-float result3 = value3 / value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+string output = "";
+foreach (var word in pangram.Split(" "))
+{
+  char[] characters = word.ToCharArray();
+  Array.Reverse(characters);
+  output += new string(characters);
+  output += " ";
+}
+Console.WriteLine(output.Trim());
