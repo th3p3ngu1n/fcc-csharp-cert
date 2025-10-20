9f63c0a0c67bebea07b3b7724e27794ad1a65bf4
th3p3ngu1n <kristin.b.harris21@gmail.com>
2025-10-19 20:26:34 -0500

    exercise - challenge string interpolation

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,12 +1,24 @@
-﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
-string[] orders = orderStream.Split(",");
-Array.Sort(orders);
-foreach (var order in orders)
-{
-  Console.Write(order);
-  if (order.Length != 4)
-  {
-    Console.Write("\t- Error");
-  }
-  Console.WriteLine("");
-}
+﻿string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+string customerLetter = $"Dear {customerName},";
+customerLetter += "\nAs a customer of our Magic Yield offering we are excited to tell you about a new financial product that would dramatically increase your return.";
+customerLetter += $"\n\nCurrently, you own {currentShares:N} shares at a return of {currentReturn:P}.";
+customerLetter += $"\n\nOur new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.";
+
+Console.WriteLine(customerLetter);
+
+string comparisonMessage = "\nHere's a quick comparison:\n";
+
+comparisonMessage += $"{currentProduct,-20}{currentReturn,-10:P2}{currentProfit,-20:C}";
+comparisonMessage += $"\n{newProduct,-20}{newReturn,-10:P2}{newProfit,-20:C}";
+
+Console.WriteLine(comparisonMessage);
